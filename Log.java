93999da23b8f648cb0c47321b5771f924b8b6275
Bin Gao <gaobin@Bins-MacBook-Pro.local>
2017-06-02 14:34:26 -0700

    log to a file

--- Log.java
+++ Log.java
@@ -1,10 +1,14 @@
 //This class polls front of the queue and log it to the target file.
 
 import java.util.LinkedList;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 public class Log implements Runnable{
     private LinkedList<Packet> queue = new LinkedList<Packet>();
-
+    private int averageProcessTime;
+    private static final String FILENAME = "logFile.txt";
 
     public Log(LinkedList<Packet> q){
         queue = q;
@@ -12,16 +16,21 @@ public class Log implements Runnable{
 
 	//Log packets to file X if queue is not empty
 	public void run(){
-		while(true){
-			try{
-				Thread.sleep(100);
-				if(queue.isEmpty() == false){
-				    System.out.println(queue.poll().GetData());
-				}
-			}catch(InterruptedException e){
-				System.out.println(e);
-			}
-
-		}
+        try (FileWriter file = new FileWriter(FILENAME)){
+    		while(true){
+    			try{
+    				Thread.sleep(100);
+    				if(queue.isEmpty() == false){
+                        Packet dequeuePacket = queue.poll();
+                        file.write(dequeuePacket.GetData()+"\n");
+                        //writer.close();
+    				}
+    			} catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+    		}
+        }catch(IOException e){
+            e.printStackTrace();
+        }
 	}
 }
