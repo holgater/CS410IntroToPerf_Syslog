5ace73d128e3d715d9bd7c3d86f7ebc7027e257b
Joe <joe@Josephs-MacBook-Air.local>
2017-06-02 13:07:21 -0700

    Now single startup
    client now runs via thread from server

--- Client.java
+++ Client.java
@@ -6,55 +6,55 @@ import java.time.LocalTime;
 import java.util.Scanner;
 
 public class Client {
-	
-	public static void main(String argv[]) throws Exception {
-		
-		//set up for local user input/output 
-		Scanner reader = new Scanner(System.in);
-		
-		//set up connection
-		System.out.println("CLIENT: Connecting...");
-		//setup socket and input/output streams
-		Socket clientSocket = new Socket("localhost", 9001);
-		ObjectOutputStream outStream = new ObjectOutputStream(clientSocket.getOutputStream());
-		ObjectInputStream inStream = new ObjectInputStream(clientSocket.getInputStream());
-		System.out.println("CLIENT: Connection established");
-		
-		
-		//setup the display from inStream
-		//Displayer display = new Displayer(inStream);
-		//run displayer in separate thread so that incoming messages don't block use
-		//Thread thread = new Thread(display);
-		//thread.start();
-		int i = 0;
-		//repeatedly send packets
-		while (i < 10) {
-			System.out.println("sent: " + i);
-			Packet pkt = new Packet();
-			pkt.SetInit(LocalTime.now());
-			pkt.SetData("Some string that takes up some space " + i);
-
-			//package properly and send to output stream
-			sendPkt(pkt, outStream);
-			++i;
-		} //end while
-		while(true) {
-			
-		}
-	} //end main
-
-	//handle the command and output the appropriate packet
-	private static void sendPkt(Packet packetOut, ObjectOutputStream outStream) {
-		try {
-			outStream.writeObject(packetOut);
-			outStream.flush();
-			outStream.reset();
-		} catch (IOException e) {
-			System.out.println("CLIENT: [ERROR] IOException while sendng message command");
-			//e.printStackTrace();
-		}
-		
-		return;
-	} //end send packet
-
+    
+    public static void main(String argv[]) throws Exception {
+        
+        //set up for local user input/output
+        Scanner reader = new Scanner(System.in);
+        
+        //set up connection
+        System.out.println("CLIENT: Connecting...");
+        //setup socket and input/output streams
+        Socket clientSocket = new Socket("localhost", 9001);
+        ObjectOutputStream outStream = new ObjectOutputStream(clientSocket.getOutputStream());
+        ObjectInputStream inStream = new ObjectInputStream(clientSocket.getInputStream());
+        System.out.println("CLIENT: Connection established");
+        
+        
+        //setup the display from inStream
+        //Displayer display = new Displayer(inStream);
+        //run displayer in separate thread so that incoming messages don't block use
+        //Thread thread = new Thread(display);
+        //thread.start();
+        int i = 0;
+        //repeatedly send packets
+        while (i < 10) {
+            System.out.println("sent: " + i);
+            Packet pkt = new Packet();
+            pkt.SetInit(LocalTime.now());
+            pkt.SetData("Some string that takes up some space " + i);
+            
+            //package properly and send to output stream
+            sendPkt(pkt, outStream);
+            ++i;
+        } //end while
+        while(true) {
+            
+        }
+    } //end main
+    
+    //handle the command and output the appropriate packet
+    private static void sendPkt(Packet packetOut, ObjectOutputStream outStream) {
+        try {
+            outStream.writeObject(packetOut);
+            outStream.flush();
+            outStream.reset();
+        } catch (IOException e) {
+            System.out.println("CLIENT: [ERROR] IOException while sendng message command");
+            //e.printStackTrace();
+        }
+        
+        return;
+    } //end send packet
+    
 }
--- Server.java
+++ Server.java
@@ -36,3 +36,60 @@ public class Server {
 	} //end main
 
 }
+
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+    
+    public static void main(String argv[]) throws Exception {
+        
+        //Handles clients
+        final ClientHandler clientHandler = new ClientHandler();
+        
+        final Log log = new Log(clientHandler.queue);
+        
+        //processing packets if queue is not empty
+        Thread logThread = new Thread(log);
+        logThread.start();
+        
+        //setup the sockets
+        ServerSocket welcomeSocket = new ServerSocket(9001);
+        Socket clientSocket = null;
+        
+        //run a client
+        Client client = new Client("prog1",200,1000);
+        Thread threadclient = new Thread(client);
+        System.out.println("SERVER: Starting up a thread client #1.");
+        threadclient.start();
+        
+        
+        //run a client
+        Client client2 = new Client("prog2",200,1000);
+        Thread threadclient2 = new Thread(client2);
+        System.out.println("SERVER: Starting up a thread client #2.");
+        threadclient2.start();
+        
+        
+        System.out.println("SERVER: Now we move forward to listen for client(s)..");
+        
+        
+        while(true)	{
+            //Connect new clients
+            clientSocket = welcomeSocket.accept();
+            System.out.println("SERVER: New client connection");
+            
+            //Create service to handle client
+            ClientService clientService = new ClientService(clientSocket, clientHandler);
+            
+            //Create new thread to run service
+            Thread thread = new Thread(clientService);
+            thread.start();
+            
+        }
+        
+    } //end main
+    
+}
