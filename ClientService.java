9207c34ddf15a6a00bbbed8a0a671c6ab1d85d36
AnarchySong <anarchysong@gmail.com>
2017-06-05 23:33:33 -0700

    Update ClientService.java

--- ClientService.java
+++ ClientService.java
@@ -23,35 +23,29 @@ public class ClientService implements Runnable {
 	public void run() {
 		//run the client service
 		try {
-			try {
-				
-				//create new input/output streams
-				inStream = new ObjectInputStream(socket.getInputStream());
-				outStream = new ObjectOutputStream(socket.getOutputStream());
-				while(true) {
-					//write back
-					//Packet response = new Packet();
-					//response.SetInit(LocalTime.now());
-					//response.SetData("Response");
-					//outStream.writeObject(response);
-					//outStream.flush();
-					//outStream.reset();
-					//service the client
-					serviceClient();
-				}
-			} finally {
-				//close the socket
-				socket.close();
-			}
+			//create new input/output streams
+			inStream = new ObjectInputStream(socket.getInputStream());
+			outStream = new ObjectOutputStream(socket.getOutputStream());
+			while(true) {
+				//write back
+				//Packet response = new Packet();
+				//response.SetInit(LocalTime.now());
+				//response.SetData("Response");
+				//outStream.writeObject(response);
+				//outStream.flush();
+				//outStream.reset();
+				//service the client
+				serviceClient();
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			//close the socket
+			socket.close();
 		}
-		
 	}
 
 	private void serviceClient() throws IOException {
 		while (true) {
-			
 			Packet pktIn;
 			//try to get incoming packet
 			try {
@@ -67,9 +61,7 @@ public class ClientService implements Runnable {
 	}
 
 	private void handlePacket(Packet pktIn) {
-
 		System.out.println("Init Time: " + pktIn.GetInit() + " - Data: " + pktIn.GetData());
 		clientHandler.addToQueue(pktIn);
-				
 	} //end handlePacket()
 }
